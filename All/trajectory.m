efe654806261a49453bcded0d1c1723fb0fa34e3
shengyu_lin <102337415+daniellsy@users.noreply.github.com>
2022-05-10 16:17:24 +0800

    Add files via upload

---
+++ All/trajectory.m
@@ -0,0 +1,9 @@
+clear;clc;
+load movedata;
+plot(t,z);
+% h = line('xdata',[],'ydata',[],'color','r','marker','.','markersize',30);
+% for ii=1:length(x)
+%     set(h,'xdata',x(ii),'ydata',y(ii));
+%     drawnow
+%     pause(0.00000000001)
+% end
